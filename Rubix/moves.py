01a21260956f2ccdb4cfe4c794299c4d1e487b01
Alex Greenhalgh <84751909+ahalgh@users.noreply.github.com>
2022-07-28 15:46:14 -0600

    Update moves.py

--- Rubix/moves.py
+++ Rubix/moves.py
@@ -24,46 +24,57 @@ def rotateFaceClockwise(cube,face):
         mat[2][2-i] = mat[i][2]
         mat[i][2] = temp
 
-    #mat is the easy part, now need to account for other portions of the rotation
-    print("Color",face.color)
+    # mat is the easy part, now need to account for other portions of the rotation
+    # print("Color",face.color)
     
-    # Returns indexs necessary for counterclockwise rotation
+    # Returns indexs necessary for counterclockwise rotation, wont be the same, just need to split off into functions 
+    # Should I use different functions? 
+    
+    adjDict = {
+        'O': rotateOside(face.color),
+        'Y': rotateYside(face.color),
+        'W': rotateWside(face.color),
+        'G': rotateGside(face.color),
+        'B': rotateBside(face.color),
+        'R': rotateRside(face.color),
+    }
+   
     adjIdx = returnAdjIdx(face.color)
     print(adjIdx)
     
     print("Above Face: ",cube.faceList[adjIdx[0]].faceNum[2,:])
-    
-    aboveFace = cube.faceList[adjIdx[0]].faceNum
-    print("aboveFace",aboveFace)
-    rightFace = cube.faceList[adjIdx[1]].faceNum
-    print("rightFace",rightFace)
-    belowFace = cube.faceList[adjIdx[2]].faceNum
-    print("belowFace",belowFace)
-    leftFace = cube.faceList[adjIdx[3]].faceNum
-    print("leftFace",leftFace)
-    # Save the value about to be erased
-    print("######################################")
-    
-    # Copy Right side of matrix
-    rightRotate = copy.copy(rightFace[:,0])
-    
-    # Overwrite Right with Above
-    rightFace[:,0] = aboveFace[2,:]
-    
-    # Copy Bottom side of matrix
-    belowRotate = copy.copy(belowFace[0,:].reshape(1,-1))
-    
-    # Overwrite bottom with Right
-    belowFace[0,:] = np.flip(rightRotate)
-    
-    # Copy left side of matrix
-    leftRotate = copy.copy(leftFace[:,2])
-    
-    # Overwrite left with bottom
-    leftFace[:,2] = belowRotate
-    
-    # Overwrite top with left
-    aboveFace[2,:] = np.flip(leftRotate)
+    def rotateWside(face.color):
+       aboveFace = cube.faceList[adjIdx[0]].faceNum
+       print("aboveFace",aboveFace)
+       rightFace = cube.faceList[adjIdx[1]].faceNum
+       print("rightFace",rightFace)
+       belowFace = cube.faceList[adjIdx[2]].faceNum
+       print("belowFace",belowFace)
+       leftFace = cube.faceList[adjIdx[3]].faceNum
+       print("leftFace",leftFace)
+       # Save the value about to be erased
+       print("######################################")
+
+       # Copy Right side of matrix
+       rightRotate = copy.copy(rightFace[:,0])
+
+       # Overwrite Right with Above
+       rightFace[:,0] = aboveFace[2,:]
+
+       # Copy Bottom side of matrix
+       belowRotate = copy.copy(belowFace[0,:].reshape(1,-1))
+
+       # Overwrite bottom with Right
+       belowFace[0,:] = np.flip(rightRotate)
+
+       # Copy left side of matrix
+       leftRotate = copy.copy(leftFace[:,2])
+
+       # Overwrite left with bottom
+       leftFace[:,2] = belowRotate
+
+       # Overwrite top with left
+       aboveFace[2,:] = np.flip(leftRotate)
     
     
     
